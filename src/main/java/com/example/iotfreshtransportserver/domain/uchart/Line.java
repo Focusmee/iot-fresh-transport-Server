50cc64ea8af8bb29684515caef6da94297976357
乌拉拉巴卡巴卡 <2105735259@qq.com>
2023-07-28 11:37:49 +0800

    基本功能实现，还需要完善前端测试端口，以及测试mqtt发送消息接收，订阅主题优化等内容。

---
+++ src/main/java/com/example/iotfreshtransportserver/domain/uchart/Line.java
@@ -0,0 +1,18 @@
+package com.example.iotfreshtransportserver.domain.uchart;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Line {
+    private List<String> categories;
+    private List<LineSeries> series;
+
+    // Getters and setters (omitted for brevity)
+}
+
